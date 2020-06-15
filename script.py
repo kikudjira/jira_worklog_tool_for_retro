5992b2bbb8c27f78baf7b20ec06828a2d7a7db82
Andrey Sekirkin <andreysek@gmail.com>
2020-06-15 23:47:39 +0300

    форматировал json

--- script.py
+++ script.py
@@ -1,12 +1,17 @@
 import pandas as pd
+import json
 import requests
 import credits
 
-url = 'https://jira.csssr.io/rest/api/2/issue/GTP-7'
-headers = { "Accept": "application/json", "Content-Type": "application/json" }
+url = 'https://jira.csssr.io/rest/api/2/issue/GTP-7/worklog'
+#headers = { "Accept": "application/json", "Content-Type": "application/json" }
 username = credits.username
 password = credits.password
-r = requests.get(url, headers=headers, auth=(username,password))
 
-df = pd.read_json(r.text)
-df.to_csv('source.csv')
+r = requests.get(url, auth=(username,password))
+data = r.json()
+with open('data.json', 'w') as f:
+    json.dump(data, f)
+
+df = pd.read_json('data.json')
+df.to_csv('data.csv')
