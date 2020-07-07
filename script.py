2c81bd8ddd334a913bd3cdea108e190f4e166035
Andrey Sekirkin <andreysek@gmail.com>
2020-07-07 22:27:36 +0300

    Отдебажил после фидбэка

--- script.py
+++ script.py
@@ -3,6 +3,7 @@ import functools
 import requests
 import credits
 import numpy as np
+from datetime import datetime
 
 
 def get_json_from_jira(url):
@@ -49,7 +50,8 @@ projectsColumns = {
 projectsDataFrame = get_data_frame_from_json(projectsData, projectsKeys, projectsColumns)
 
 # Вводим название или ID проекта и проверяем есть ли у нас такой
-# project = 'Smartbe'
+
+# project = 'Palex'
 project = input('Project: ')
 
 while not projectsDataFrame.isin([project]).any().any():
@@ -57,22 +59,22 @@ while not projectsDataFrame.isin([project]).any().any():
     project = input('Project: ')
 
 # Вводим период времени и конверитим в unix timestamp
-# dateFrom = '27, 06, 2020'.split(', ')
-# dateTo = '03, 06, 2020'.split(', ')
 
+# dateFrom = '05, 07, 2020'.split(', ')
 dateFrom = input('From Date, Month, Year: ').split(', ')
-# dateTo = input('To Date, Month, Year: ').split(', ')
 
-# Получаем DataFrame не дев сотрудников
-notDevURL = 'https://jira.csssr.io/rest/api/2/group/member?groupname=csssr-notdev'
-notDevData = get_json_from_jira(notDevURL)['values']
+# Получаем DataFrame не дев сотрудников (может только админ джиры)
+# notDevURL = 'https://jira.csssr.io/rest/api/2/group/member?groupname=csssr-notdev'
+# notDevData = get_json_from_jira(notDevURL)['values']
+#
+# notDevKeys = ['displayName'], ['key']
+# notDevColumns = {
+#     0: 'Name',
+#     1: 'Key'}
+#
+# notDevDataFrame = get_data_frame_from_json(notDevData, notDevKeys, notDevColumns).to_csv('notDev.csv')
 
-notDevKeys = ['displayName'], ['key']
-notDevColumns = {
-    0: 'Name',
-    1: 'Key'}
-
-notDevDataFrame = get_data_frame_from_json(notDevData, notDevKeys, notDevColumns)
+notDevDataFrame = pd.read_csv('notDev.csv')
 
 # Получаем задачи за указанный период времени
 'https://jira.csssr.io/rest/api/2/search?maxResults=500&jql=project%20%3D%20GAZ-MPSS%20and%20updated%20%3E%3D%20%222020%2F07%2F01%2000%3A00%22'
@@ -103,36 +105,57 @@ for index, a in issuesInPeriodDataFrame.iterrows():
     if a['Components'] != [] and a['Components'][0]['name'] == 'do_not_analyze':
         issuesInPeriodDataFrame.at[index, 'Components'] = 'do_not_analyze'
 
-issuesInPeriodDataFrame = issuesInPeriodDataFrame.loc[issuesInPeriodDataFrame['Components'] != 'do_not_analyze']
+issuesDataFrame = issuesInPeriodDataFrame.loc[issuesInPeriodDataFrame['Components'] != 'do_not_analyze']
 
 # Вытаскиваем все ворклоги из каждой задачи
 print('Начинаю вытаскивать ворклоги из всех задач...')
 totalWorklogsData = []
 n = 0
-n_index = len(issuesInPeriodDataFrame.index)
-for issue in issuesInPeriodDataFrame['Issue Id']:
+n_index = len(issuesDataFrame.index)
+for issue in issuesDataFrame['Issue Id']:
     n += 1
     print(n, " from ", n_index )
     getWorklogFromIssueURL = 'https://jira.csssr.io/rest/api/2/issue/' + issue + '/worklog'
     partWorklogs = get_json_from_jira(getWorklogFromIssueURL)
     totalWorklogsData += partWorklogs['worklogs']
 
-totalWorklogsKeys = ['issueId'], ['author', 'displayName'], ['timeSpentSeconds']
+totalWorklogsKeys = ['issueId'], ['author', 'displayName'], ['timeSpentSeconds'], ['created']
 totalWorklogsColumns = {
     0: 'Issue Id',
     1: 'Author',
-    2: 'Time Spent'
+    2: 'Time Spent',
+    3: 'Worklog created'
 }
 
 totalWorklogsDataFrame = get_data_frame_from_json(totalWorklogsData, totalWorklogsKeys, totalWorklogsColumns)
 totalWorklogsDataFrame = totalWorklogsDataFrame.loc[totalWorklogsDataFrame['Author'].isin(notDevDataFrame['Name']) == False]
+
+# Делаем датафрейм с ворклогами в периоде указанной даты
+timestampFrom = str(int(datetime(int(dateFrom[2]), int(dateFrom[1]), int(dateFrom[0])).timestamp()))
+
+for index, a in totalWorklogsDataFrame.iterrows():
+    totalWorklogsDataFrame.at[index, 'Worklog created'] = str(int(datetime(int(a['Worklog created'][:4]),
+                                                                           int(a['Worklog created'][5:7]),
+                                                                           int(a['Worklog created'][8:10]))
+                                                                  .timestamp()))
+
+periodWorklogsDataFrame = totalWorklogsDataFrame.loc[totalWorklogsDataFrame['Worklog created'] > timestampFrom]
+periodWorklogsDataFrame = periodWorklogsDataFrame.drop(
+    columns=['Author', 'Worklog created']).rename(
+    columns={'Time Spent': 'Time Spent Period'})
+
+periodWorklogsDataFrame['Time Spent Period'] = (periodWorklogsDataFrame['Time Spent Period'] / 60) / 60
+
+periodWorklogsDataFramePivot = periodWorklogsDataFrame.pivot_table(index=['Issue Id'], values=['Time Spent Period'],
+                                                                   aggfunc=np.sum)
 
 # Теперь мерджим дата фреймы финально
 finalDataFrame = totalWorklogsDataFrame.merge(issuesInPeriodDataFrame, on='Issue Id', how='outer')
 finalDataFrame = finalDataFrame.fillna(0)
 finalDataFrame = finalDataFrame.loc[finalDataFrame['Time Spent'] != 0.0]
 
-userDataFrame = finalDataFrame.drop(columns=['Issue Key', 'Time Spent', 'Issue Type', 'Components', 'Project', 'Summary', 'Original Estimate'])
+userDataFrame = finalDataFrame.drop(columns=['Issue Key', 'Time Spent', 'Issue Type', 'Components', 'Project',
+                                             'Summary', 'Original Estimate'])
 userDataFrame = userDataFrame.groupby('Issue Id')['Author'].apply(lambda x: list(np.unique(x))).reset_index()
 userDataFrame['Author'] = userDataFrame['Author'].apply(lambda x: ','.join(map(str, x)))
 
@@ -143,11 +166,15 @@ finalDataFrame['Issue Key'] = 'https://jira.csssr.io/browse/' + finalDataFrame['
 finalDataFrame['Original Estimate'] = (finalDataFrame['Original Estimate'] / 60) / 60
 finalDataFrame['Time Spent'] = (finalDataFrame['Time Spent'] / 60) / 60
 
-for index, a in finalDataFrame.iterrows():
-    finalDataFrame.at[index, 'Time Spent'] = float("{0:.2f}".format(a['Time Spent']))
+finalDataFramePivot = finalDataFrame.pivot_table(index=['Issue Id', 'Issue Key', 'Summary', 'Issue Type',
+                                                        'Worklog Authors', 'Original Estimate'],
+                                                 values=['Time Spent'],
+                                                 aggfunc=np.sum)
 
-finalDataFramePivot = finalDataFrame.pivot_table(index=['Issue Key', 'Summary', 'Issue Type', 'Worklog Authors', 'Original Estimate'],
-                                                 values=['Time Spent'], aggfunc=np.sum)
 finalDataFrame = finalDataFramePivot.reindex(finalDataFramePivot.sort_values(by='Issue Type', ascending=False).index)
+finalDataFrame = finalDataFrame.join(periodWorklogsDataFramePivot, on='Issue Id', how='outer').reset_index()
+finalDataFrame = finalDataFrame.drop(columns=['Issue Id'])
+finalDataFrame = finalDataFrame.loc[finalDataFrame['Time Spent Period'].isnull() == False]
+finalDataFrame = finalDataFrame.round(2)
 
 finalDataFrame.to_csv('result.csv')
